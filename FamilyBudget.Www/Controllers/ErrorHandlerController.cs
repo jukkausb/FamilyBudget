c90cdcb1fa95dd8466d63065d2c698a02e6d88f3
Artem Palamarchuk <jukkausb@mail.ru>
2015-12-23 14:20:59 +0300

    Messing files

---
+++ FamilyBudget.Www/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,15 @@
+﻿using FamilyBudget.Www.App_CodeBase;
+using System;
+using System.Web.Mvc;
+
+namespace FamilyBudget.Www.Controllers
+{
+    public class ErrorHandlerController : Controller
+    {
+        public ActionResult Index()
+        {
+            Exception warning = GlobalExceptionHandler.GetApplicationWarning();
+            return warning != null ? PartialView("Warning", warning.Message) : null;
+        }
+    }
+}
