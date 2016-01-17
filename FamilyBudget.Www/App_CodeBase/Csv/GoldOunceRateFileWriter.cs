4f6bdfd07cc7402eceb891e61e46339f92189e36
Artem Palamarchuk <jukkausb@mail.ru>
2016-01-17 21:39:03 +0300

    Check in missing files

---
+++ FamilyBudget.Www/App_CodeBase/Csv/GoldOunceRateFileWriter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace FamilyBudget.Www.App_CodeBase.Csv
+{
+    public static class GoldOunceRateFileWriter
+    {
+        private static readonly string FileNameFormat = HttpContext.Current.Server.MapPath(@"~/App_Data/gold-ounce-rates-{0}.csv");
+
+        public static void SaveRatesToFile(string currency, string ratesCsvText)
+        {
+            try
+            {
+                string fileName = string.Format(FileNameFormat, currency);
+
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a file to write to.
+                using (var sw = File.CreateText(string.Format(FileNameFormat, currency)))
+                {
+                    sw.WriteLine(ratesCsvText);
+                }
+            }
+            catch (Exception ex)
+            {
+                GlobalExceptionHandler.SetApplicationWarning(ex);
+                throw;
+            }
+        }
+
+        public static string ReadRatesFromFile(string currency)
+        {
+            try
+            {
+                // Open the file to read from.
+                string s;
+                using (StreamReader sr = File.OpenText(string.Format(FileNameFormat, currency)))
+                {
+                    s = sr.ReadLine();
+                }
+
+                return s;
+            }
+            catch (Exception ex)
+            {
+                GlobalExceptionHandler.SetApplicationWarning(ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ FamilyBudget.Www/App_CodeBase/GoldOunceRatePreloader.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Practices.ServiceLocation;
+
+namespace FamilyBudget.Www.App_CodeBase
+{
+    public static class GoldOunceRatePreloader
+    {
+        private static IGoldOunceRateProvider _goldOunceRateProvider;
+
+        public static void Preload()
+        {
+            _goldOunceRateProvider = ServiceLocator.Current.GetInstance<IGoldOunceRateProvider>();
+
+            _goldOunceRateProvider.DownloadGoldOunceRates("USD");
+        }
+    }
+}
